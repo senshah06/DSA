428930175565de1fcd998a30761c0a8175365b2c
Shahil Sen <82971335+senshah06@users.noreply.github.com>
2025-06-19 18:19:09 +0530

    Create MaxAreaInGraph.java

---
+++ MaxAreaInGraph.java
@@ -0,0 +1,64 @@
+// You are given an integer array height of length n. There are n vertical lines drawn such that the two endpoints of the ith line are (i, 0) and (i, height[i]).
+// Find two lines that together with the x-axis form a container, such that the container contains the most water.
+// Return the maximum amount of water a container can store.
+// Notice that you may not slant the container.
+
+// Example 1:
+// Image for Reference(Example 1) -> https://s3-lc-upload.s3.amazonaws.com/uploads/2018/07/17/question_11.jpg
+// Input: height = [1,8,6,2,5,4,8,3,7]
+								 
+// Output: 49
+// Explanation: The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7]. In this case, the max area of water (blue section) the container can contain is 49.
+
+// Example 2:
+// Input: height = [1,1]
+// Output: 1
+ 
+
+// Constraints:
+// n == height.length
+// 2 <= n <= 105
+// 0 <= height[i] <= 104
+
+
+//O(n) approach
+public class Solution {
+    public int maxArea(int[] height) {
+        int left = 0, right = height.length - 1;
+        int maxArea = 0;
+
+        while (left < right) {
+            int h = Math.min(height[left], height[right]);
+            int w = right - left;
+            int area = h * w;
+            maxArea = Math.max(maxArea, area);
+
+            // Move the pointer pointing to the shorter line
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return maxArea;
+    }
+}
+//O(n2) approach
+public class Solution {
+    public int maxArea(int[] height) {
+        int maxArea = 0;
+        int n = height.length;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int h = Math.min(height[i], height[j]);
+                int w = j - i;
+                int area = h * w;
+                maxArea = Math.max(maxArea, area);
+            }
+        }
+
+        return maxArea;
+    }
+}
